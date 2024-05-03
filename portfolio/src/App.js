bdebe1239e5302cceedd131366f0e9be4aa19815
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-05-03 00:18:03 -0700

    testing netlify

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h4>hi</h4>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
