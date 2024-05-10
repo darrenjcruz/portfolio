5ab3f72574f0405dc8b849e03700a14344873aa1
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-05-09 18:25:53 -0700

    Navbar created

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
+import NaviBar from './components/navibar/NaviBar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>hi</h4>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NaviBar />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            
+                <h4>hi</h4>
+                
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
